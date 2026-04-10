1ba9ff74c40432bc3c9204fd9e49f346b274a7d6
Jinchao Lin <jinchao.1926@gmail.com>
2026-04-10 17:33:35 +0800

    [API] Add trusted origins for better-auth

--- nexchat-api/src/lib/auth.ts
+++ nexchat-api/src/lib/auth.ts
@@ -2,9 +2,17 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { openAPI } from 'better-auth/plugins';
 
+import {
+  APP_BASE_URL,
+  AUTH_PREFIX,
+  BETTER_AUTH_TRUSTED_ORIGINS,
+} from '@/config';
 import { db } from '@/db';
 
 export const auth = betterAuth({
+  basePath: AUTH_PREFIX,
+  baseURL: APP_BASE_URL,
+  trustedOrigins: BETTER_AUTH_TRUSTED_ORIGINS,
   database: drizzleAdapter(db, {
     provider: 'sqlite',
   }),
