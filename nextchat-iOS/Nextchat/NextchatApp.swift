09b5493de17ec704ea22d707773dc9471dee9e8a
Jinchao Lin <51687782+Jinchao1926@users.noreply.github.com>
2026-04-16 15:44:46 +0800

    [iOS] Init SwiftUI (#23)

---
+++ nextchat-iOS/Nextchat/NextchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextchatApp.swift
+//  Nextchat
+//
+//  Created by Jinchao Lin on 2026/4/16.
+//
+
+import SwiftUI
+
+@main
+struct NextchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
