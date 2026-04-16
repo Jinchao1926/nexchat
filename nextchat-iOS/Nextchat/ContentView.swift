b8f237014fcf7d8bcf87575dfdb21e46a7a9798c
Jinchao Lin <jinchao.1926@gmail.com>
2026-04-16 15:44:21 +0800

    [iOS] Init SwiftUI

---
+++ nextchat-iOS/Nextchat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nextchat
+//
+//  Created by Jinchao Lin on 2026/4/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
